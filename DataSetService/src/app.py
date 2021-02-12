bdc799332c924676e5fe23167caa558e6a3a3783
Konstantin Strümpf <k.struempf@icloud.com>
2021-02-12 11:47:46 +0100

    [HT-49] Added endpoints to in-memory dataset store for loading by hash and storing datasets

--- DataSetService/src/app.py
+++ DataSetService/src/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+import os
+
+import pandas as pd
+from flask import Flask, render_template, request, abort
 from flask_bootstrap import Bootstrap
 from flask_jsonpify import jsonpify
 from flask_socketio import SocketIO
 
 from services.in_memory_store import InMemoryStore
+
+PORT: int = os.getenv("PORT", 5000)
 
 app = Flask(__name__, template_folder='templates')
 socketio = SocketIO(app)
@@ -20,19 +25,37 @@ def root():
         })
 
 
-@app.route('/api/datasets/<dataset_id>')
-def get_dataset_by_id(dataset_id: str):
+@app.route('/api/datasets/<dataset_id>', methods=['GET'])
+def dataset_by_id(dataset_id: str):
     df = store.get_by_id(dataset_id)
 
     if df is None:
-        return None
+        abort(404)
 
     df_list = df.values.tolist()
     return jsonpify(df_list)
+
+
+@app.route('/api/datasets/hash/<producing_hash>', methods=['GET', 'POST'])
+def dataset_by_hash(producing_hash: str):
+    if request.method == 'GET':
+        df = store.get_by_hash(producing_hash)
+
+        if df is None:
+            abort(404)
+
+        df_list = df.values.tolist()
+        return jsonpify(df_list)
+
+    if request.method == 'POST':
+        data = request.data
+        df = pd.read_json(data)
+        store.store_data_set(producing_hash, df)
+        return 'OK'
 
 
 store.import_with_id("../datasets/Melbourne_housing_FULL.csv", "00e61417-cada-46db-adf3-a5fc89a3b6ee")
 store.import_with_id("../datasets/MELBOURNE_HOUSE_PRICES_LESS.csv", "0c2acbdb-544b-4efc-ae54-c2dcba988654")
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, use_reloader=False, debug=True)
+    socketio.run(app, host='0.0.0.0', port=PORT, use_reloader=False, debug=True)
--- DataSetService/src/services/in_memory_store.py
+++ DataSetService/src/services/in_memory_store.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 import pandas as pd
@@ -9,12 +10,17 @@ class InMemoryStore:
     def __init__(self) -> None:
         super().__init__()
         self.store = dict()
+        self.logger = logging.getLogger('InMemoryStore')
+        handler = logging.StreamHandler()
+        handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(message)s"))
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.INFO)
 
     def import_with_id(self, file: str, dataframe_id: str):
         dirname = os.path.dirname(__file__)
         filename = os.path.join(dirname, '../', file)
 
-        print('Importing %s and storing with id %s' % (filename, str(dataframe_id)))
+        self.logger.info('Importing %s and storing with id %s' % (filename, str(dataframe_id)))
 
         df = pd.read_csv(filename)
         self.store[dataframe_id] = df
@@ -23,9 +29,21 @@ class InMemoryStore:
         return len(self.store)
 
     def get_by_id(self, dataframe_id: str):
-        print("Loading dataset by id %s" % str(dataframe_id))
+        self.logger.info("Loading dataset by id %s" % str(dataframe_id))
 
         if self.store.keys().__contains__(dataframe_id):
             return self.store.get(dataframe_id)
         else:
             return None
+
+    def get_by_hash(self, producing_hash):
+        self.logger.info("Loading dataset by hash %s" % str(producing_hash))
+
+        if self.store.keys().__contains__(producing_hash):
+            return self.store.get(producing_hash)
+        else:
+            return None
+
+    def store_data_set(self, key: str, data):
+        self.logger.info("Storing data with key %s of shape %s" % (key, data.shape))
+        self.store[key] = data
