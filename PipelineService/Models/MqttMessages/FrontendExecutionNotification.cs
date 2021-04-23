7ffd1e433ce8daf3a30f302924b26ae3d8dfb353
Konstantin Strümpf <k.struempf@icloud.com>
2021-04-23 13:20:35 +0200

    [PH-135] Added failed nodes to frontend notification and setting start time of execution

--- PipelineService/Models/MqttMessages/FrontendExecutionNotification.cs
+++ PipelineService/Models/MqttMessages/FrontendExecutionNotification.cs
@@ -12,7 +12,8 @@ namespace PipelineService.Models.MqttMessages
         public string ErrorDescription { get; set; }
         public int NodesExecuted { get; set; }
         public int NodesInExecution { get; set; }
-        public int ToBeExecuted { get; set; }
+        public int NodesToBeExecuted { get; set; }
+        public int NodesFailedToExecute { get; set; }
         public string OperationName { get; set; }
         public string ResultDatasetKey { get; set; }
         public DateTime CompletedAt { get; set; }
