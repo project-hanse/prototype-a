d43df3160d40765f0112d559c20717643728e2a1
Konstantin Strümpf <k.struempf@icloud.com>
2021-09-12 13:29:15 +0200

    [PH-199] Fixed unit tests

--- PipelineService.UnitTests/Services/PipelineDtoServiceUnitTests.cs
+++ PipelineService.UnitTests/Services/PipelineDtoServiceUnitTests.cs
@@ -1,8 +1,11 @@
+using System;
 using System.Threading.Tasks;
+using Moq;
 using NUnit.Framework;
 using PipelineService.Dao;
 using PipelineService.Dao.Impl;
 using PipelineService.Models.Pipeline;
+using PipelineService.Models.Pipeline.Execution;
 using PipelineService.Services;
 using PipelineService.Services.Impl;
 using PipelineService.UnitTests.UnitTestHelpers;
@@ -35,18 +38,28 @@ namespace PipelineService.UnitTests.Services
         };
 
         private IPipelineDao _pipelineDao;
+        private IPipelineExecutionDao _pipelineExecutionDao;
         private IPipelineDtoService _pipelineDtoService;
 
         [SetUp]
         public void SetUp()
         {
+            var mock = new Mock<IPipelineExecutionDao>();
+            mock.Setup(dao => dao.GetLastExecutionForPipeline(It.IsAny<Guid>()))
+                .ReturnsAsync((Guid pipelineId) => new PipelineExecutionRecord
+                {
+                    PipelineId = pipelineId,
+                    CompletedOn = DateTime.UtcNow
+                });
+            _pipelineExecutionDao = mock.Object;
+
             _pipelineDao = new InMemoryPipelineDao(GeneralHelper.CreateLogger<InMemoryPipelineDao>());
-            _pipelineDtoService = new PipelineDtoService(_pipelineDao);
+            _pipelineDtoService = new PipelineDtoService(_pipelineDao, _pipelineExecutionDao);
         }
 
         [Test]
         [TestCaseSource(nameof(_getNodeTupleTestCases))]
-        public async Task GetSingleInputNodeTuples(NodeTupleTestCase testCase)
+        public async Task GetSingleInputNodeTuples_ExecutedPipeline(NodeTupleTestCase testCase)
         {
             // arrange
             await _pipelineDao.Add(testCase.Pipeline);
