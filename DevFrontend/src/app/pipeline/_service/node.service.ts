da5e30f6b787a1955d6e7281891dbecdab983512
Konstantin Strümpf <k.struempf@icloud.com>
2021-10-07 15:48:30 +0200

    [PH-198] Editing configurations in frontend

--- DevFrontend/src/app/pipeline/_service/node.service.ts
+++ DevFrontend/src/app/pipeline/_service/node.service.ts
@@ -36,7 +36,11 @@ export class NodeService extends BaseHttpService {
     return this.httpClient.get('http://localhost:5002/api/datasets/hash/describe/html/' + hash, {responseType: 'text'});
   }
 
-  public getConfig(pipelineId: string, nodeId: string): Observable<any> {
-    return this.httpClient.get(this.getUrl(pipelineId, nodeId, 'config'));
+  public getConfig(pipelineId: string, nodeId: string): Observable<Map<string, string>> {
+    return this.httpClient.get<Map<string, string>>(this.getUrl(pipelineId, nodeId, 'config'));
+  }
+
+  public updateConfig(pipelineId: string, nodeId: string, config: Map<string, string>): Observable<any> {
+    return this.httpClient.post(this.getUrl(pipelineId, nodeId, 'config'), config);
   }
 }
--- DevFrontend/src/app/pipeline/node-config-editor/node-config-editor.component.ts
+++ DevFrontend/src/app/pipeline/node-config-editor/node-config-editor.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Observable, Subscription} from 'rxjs';
 import {NodeService} from '../_service/node.service';
 
 @Component({
@@ -7,26 +7,38 @@ import {NodeService} from '../_service/node.service';
   templateUrl: './node-config-editor.component.html',
   styleUrls: ['./node-config-editor.component.scss']
 })
-export class NodeConfigEditorComponent implements OnInit {
+export class NodeConfigEditorComponent implements OnInit, OnDestroy {
   @Input()
   pipelineId?: string;
   nodeIds?: Array<string>;
-  $configs: Array<Observable<any>> = [];
+  $configs: Array<Observable<Map<string, string>>> = [];
+
+  private readonly subscriptions: Subscription;
 
   constructor(private nodeService: NodeService) {
+    this.subscriptions = new Subscription();
   }
 
   ngOnInit(): void {
   }
 
-  getConfig(nodeId: string): Observable<any> {
+  getConfig(nodeId: string): Observable<Map<string, string>> {
     if (!this.$configs[nodeId]) {
       this.$configs[nodeId] = this.nodeService.getConfig(this.pipelineId, nodeId);
     }
     return this.$configs[nodeId];
   }
 
-  stringify(config: any): string {
-    return JSON.stringify(config);
+  onSubmit(nodeId: string, config: Map<string, string>): void {
+    this.subscriptions.add(
+      this.nodeService.updateConfig(this.pipelineId, nodeId, config).subscribe(
+        res => console.log(res),
+        err => console.error(err)
+      )
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
