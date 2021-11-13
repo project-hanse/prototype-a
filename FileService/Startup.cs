b84f08ddc2a00f4488b27b0afa1f7e55f77b673c
Konstantin Strümpf <k.struempf@icloud.com>
2021-11-13 14:20:34 +0100

    chore(persistence): add S3 health check to FileService
    Refs: PH-207

--- FileService/Startup.cs
+++ FileService/Startup.cs
@@ -1,3 +1,4 @@
+using Amazon.S3;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -21,6 +22,21 @@ namespace FileService
 		{
 			services.AddControllers();
 			services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "FileService", Version = "v1" }); });
+
+			var config = new AmazonS3Config
+			{
+				ForcePathStyle = Configuration.GetValue("S3Configuration:ForcePathStyle", true),
+				ServiceURL =
+					$"{Configuration.GetValue("S3Configuration:Protocol", "http")}://{Configuration.GetValue("S3Configuration:Host", "localstack-s3")}:{Configuration.GetValue("S3Configuration:Port", 4566)}"
+			};
+
+			services.AddHealthChecks().AddS3(options =>
+			{
+				options.S3Config = config;
+				options.BucketName = Configuration.GetValue("S3Configuration:DefaultBucketName", "defaultfiles");
+				options.AccessKey = Configuration.GetValue<string>("S3Configuration:AccessKey", null);
+				options.SecretKey = Configuration.GetValue<string>("S3Configuration:SecretKey", null);
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,7 +61,11 @@ namespace FileService
 
 			app.UseAuthorization();
 
-			app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+				endpoints.MapHealthChecks("/health");
+			});
 		}
 	}
 }
