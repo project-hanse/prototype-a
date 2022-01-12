78e055c2ed5ed4315f447b16df0f6dc60601cba0
Konstantin Strümpf <k.struempf@icloud.com>
2022-01-12 11:35:17 +0100

    refactor(dev-frontend): rename pipeline detail view to pipeline editor
    Refs: PH-219

--- DevFrontend/src/app/pipeline/pipeline.module.ts
+++ DevFrontend/src/app/pipeline/pipeline.module.ts
@@ -3,26 +3,26 @@ import {NgModule} from '@angular/core';
 import {CoreModule} from '../core/core.module';
 import {OperationConfigEditorComponent} from './operation-config-editor/operation-config-editor.component';
 import {OperationResultPreviewComponent} from './operation-result-preview/operation-result-preview.component';
-import {PipelineDetailViewComponent} from './pipeline-detail-view/pipeline-detail-view.component';
+import {PipelineCreateComponent} from './pipeline-create/pipeline-create.component';
+import {PipelineEditorComponent} from './pipeline-editor/pipeline-editor.component';
 import {PipelineExecutionLogComponent} from './pipeline-execution-log/pipeline-execution-log.component';
 import {PipelineGraphComponent} from './pipeline-graph-component/pipeline-graph.component';
 import {PipelineListViewComponent} from './pipeline-list-view/pipeline-list-view.component';
 
 import {PipelineRoutingModule} from './pipeline-routing.module';
 import {PipelineToolboxComponent} from './pipeline-toolbox/pipeline-toolbox.component';
-import { PipelineCreateComponent } from './pipeline-create/pipeline-create.component';
 
 
 @NgModule({
 	declarations: [
 		PipelineListViewComponent,
-		PipelineDetailViewComponent,
+		PipelineEditorComponent,
 		PipelineGraphComponent,
 		PipelineExecutionLogComponent,
 		PipelineToolboxComponent,
 		OperationResultPreviewComponent,
 		OperationConfigEditorComponent,
-  PipelineCreateComponent
+		PipelineCreateComponent
 	],
 	imports: [
 		CommonModule,
