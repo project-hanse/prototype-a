251c04e3445557a73f7990baf1f350e4cceb45e5
Konstantin Strümpf <k.struempf@icloud.com>
2022-03-13 11:55:32 +0100

    refactor: install apoc plugin in Dockerfile
    No longer manually copying .jar in extension script

--- Neo4jPlugins/scripts/neo4j_config_extension.sh
+++ Neo4jPlugins/scripts/neo4j_config_extension.sh
@@ -1,8 +1,8 @@
 # An extension script executed before neo4j db starts.
 # See https://neo4j.com/docs/operations-manual/4.4/docker/configuration/
 
-echo "Moving apoc .jar to plugins directory"
-mv "$NEO4J_HOME"/labs/apoc-*.jar "$NEO4J_HOME"/plugins/
+# echo "Moving apoc .jar to plugins directory"
+# mv "$NEO4J_HOME"/labs/apoc-*.jar "$NEO4J_HOME"/plugins/
 
 echo "Enabling file import and export"
 
@@ -11,5 +11,5 @@ echo "#********************************************************************" >>"
 echo "# Custom Neo4j configurations" >>"$NEO4J_HOME"/conf/neo4j.conf
 echo "#********************************************************************" >>"$NEO4J_HOME"/conf/neo4j.conf
 echo "" >>"$NEO4J_HOME"/conf/neo4j.conf
-echo "apoc.export.file.enabled=true" >>"$NEO4J_HOME"/conf/neo4j.conf
+# echo "apoc.export.file.enabled=true" >>"$NEO4J_HOME"/conf/neo4j.conf
 # echo "apoc.import.file.enabled=true" >>"$NEO4J_HOME"/conf/neo4j.conf
