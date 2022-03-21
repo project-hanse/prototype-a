2219a3ce4d9fe2f5f37d1e41e66c0db52fa9a400
Konstantin Strümpf <k.struempf@icloud.com>
2022-03-21 17:38:55 +0100

    fix: create bucket for mlflow before starting server

--- MLflowServer/entrypoint.sh
+++ MLflowServer/entrypoint.sh
@@ -1,3 +1,6 @@
-echo "Starting MLflow server..."
+echo "Creating S3 bucket..."
+curl -X PUT -u "$AWS_ACCESS_KEY_ID":"$AWS_SECRET_ACCESS_KEY" "${MLFLOW_S3_ENDPOINT_URL}/mlflow-artifacts"
 
-mlflow server --backend-store-uri mysql+pymysql://"${MYSQL_USER}":"${MYSQL_PASSWORD}"@"${MYSQL_HOST}":"${MYSQL_PORT}"/"${MYSQL_DATABASE}" --default-artifact-root s3://mlflow --host 0.0.0.0
+echo ""
+echo "Starting MLflow server..."
+mlflow server --backend-store-uri mysql+pymysql://"${MYSQL_USER}":"${MYSQL_PASSWORD}"@"${MYSQL_HOST}":"${MYSQL_PORT}"/"${MYSQL_DATABASE}" --default-artifact-root s3://mlflow-artifacts --host 0.0.0.0
