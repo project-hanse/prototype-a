752f4c888fac0bf8c485c9cf1ead082a9c59d851
Konstantin Strümpf <k.struempf@icloud.com>
2022-03-22 21:17:38 +0100

    fix: use dummy credentials for storing models in mlflow
    Refs: PH-94

--- LearningService/src/app.py
+++ LearningService/src/app.py
@@ -16,8 +16,8 @@ from src.services.trainer_registry import TrainerRegistry
 PORT: int = os.getenv("PORT", 5006)
 S3_HOST: str = os.getenv("S3_HOST", "localstack")
 S3_PORT: str = os.getenv("S3_PORT", "4566")
-S3_ACCESS_KEY_SECRET: str = os.getenv("S3_ACCESS_KEY_SECRET", "")
-S3_ACCESS_KEY_ID: str = os.getenv("S3_ACCESS_KEY_ID", "")
+S3_ACCESS_KEY_SECRET: str = os.getenv("S3_ACCESS_KEY_SECRET", "localstack")
+S3_ACCESS_KEY_ID: str = os.getenv("S3_ACCESS_KEY_ID", "localstack")
 MLFLOW_TRACKING_URI: str = os.getenv("MLFLOW_TRACKING_URI", "http://mlflow-server:5005")
 MLFLOW_REGISTRY_URI: str = os.getenv("MLFLOW_REGISTRY_URI", "http://mlflow-server:5005")
 PIPELINE_SERVICE_HOST: str = os.getenv("PIPELINE_SERVICE_HOST", "pipeline-service")
@@ -25,18 +25,18 @@ PIPELINE_SERVICE_PORT: int = os.getenv("PIPELINE_SERVICE_PORT", 5000)
 DATASET_SERVICE_HOST: str = os.getenv("DATASET_SERVICE_HOST", "dataset-service")
 DATASET_SERVICE_PORT: int = os.getenv("DATASET_SERVICE_PORT", 5002)
 
-# Set environment variables for boto3
+# Configure mlflow
 os.environ["AWS_ACCESS_KEY_ID"] = S3_ACCESS_KEY_ID
 os.environ["AWS_SECRET_ACCESS_KEY"] = S3_ACCESS_KEY_SECRET
 os.environ["MLFLOW_S3_ENDPOINT_URL"] = f"http://{S3_HOST}:{S3_PORT}"
+mlflow.set_registry_uri(MLFLOW_REGISTRY_URI)
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
 # Creating service instances
 app = Flask(__name__, template_folder='templates')
 CORS(app)
 socketio = SocketIO(app)
 bootstrap = Bootstrap(app)
-mlflow.set_registry_uri(MLFLOW_REGISTRY_URI)
-mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 mlflow_client = MlflowClient(tracking_uri=MLFLOW_TRACKING_URI, registry_uri=MLFLOW_REGISTRY_URI)
 pipeline_service_client = PipelineClient(host=PIPELINE_SERVICE_HOST, port=PIPELINE_SERVICE_PORT)
 dataset_client = DatasetClient(host=DATASET_SERVICE_HOST, port=DATASET_SERVICE_PORT)
