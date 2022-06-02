35fbd4b10cfd572196882920f7e230597a0b0cba
Konstantin Strümpf <k.struempf@icloud.com>
2022-06-02 12:20:06 +0200

    fix(learning-service): infer model signature based on random sample of training data

--- LearningService/src/helper/mlflow_helper.py
+++ LearningService/src/helper/mlflow_helper.py
@@ -40,17 +40,24 @@ def _infer_signature_custom(model_input, model_output):
 		return None
 
 
-def infer_signature_custom(model_input, model_output):
+def infer_signature_custom(model_input: [dict], model_output: list):
 	"""
 	Infer the signature of the model.
 	Tries to use mlflow.infer_signature(), if this does not work try custom implementation for dicts.
 	"""
+	# find index of model_input with most keys
+	max_keys = 0
+	max_index = 0
+	for i, d in enumerate(model_input):
+		if len(d) > max_keys:
+			max_keys = len(d)
+			max_index = i
 	try:
-		return infer_signature(model_input, model_output)
+		return infer_signature(model_input[max_index], model_output[max_index])
 	except Exception as e:
 		logging.info("Could not infer signature. Trying custom implementation.")
 		try:
-			return _infer_signature_custom(model_input, model_output)
+			return _infer_signature_custom(model_input[max_index], model_output[max_index])
 		except Exception as e:
 			logging.warning("Could not infer signature. Returning None.")
 			return None
