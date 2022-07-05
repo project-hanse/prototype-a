7047940b7f80831c2191a84a676314eb1a077bf2
Konstantin Strümpf <k.struempf@icloud.com>
2022-07-05 16:27:47 +0200

    chore: execute pipelines before triggering model training
    Refs: PH-244

--- PipelineService/Services/Impl/LearningServiceClient.cs
+++ PipelineService/Services/Impl/LearningServiceClient.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using PipelineService.Dao;
 using PipelineService.Extensions;
 
 namespace PipelineService.Services.Impl;
@@ -12,6 +13,8 @@ public class LearningServiceClient : ILearningServiceClient
 	private readonly ILogger<LearningServiceClient> _logger;
 	private readonly IConfiguration _configuration;
 	private readonly IHttpClientFactory _clientFactory;
+	private readonly IPipelineExecutionService _pipelineExecutionService;
+	private readonly IPipelinesDao _pipelinesDao;
 
 	private HttpClient _client;
 
@@ -27,18 +30,25 @@ public class LearningServiceClient : ILearningServiceClient
 	}
 
 
-	public LearningServiceClient(ILogger<LearningServiceClient> logger,
+	public LearningServiceClient(
+		ILogger<LearningServiceClient> logger,
 		IConfiguration configuration,
-		IHttpClientFactory clientFactory)
+		IHttpClientFactory clientFactory,
+		IPipelineExecutionService pipelineExecutionService,
+		IPipelinesDao pipelinesDao)
 	{
 		_logger = logger;
 		_configuration = configuration;
 		_clientFactory = clientFactory;
+		_pipelineExecutionService = pipelineExecutionService;
+		_pipelinesDao = pipelinesDao;
 	}
 
 	public async Task TriggerModelTraining()
 	{
 		_logger.LogDebug("Triggering model training in learning service");
+
+		await AssertAllPipelinesExecuted();
 		var request = new HttpRequestMessage(HttpMethod.Get, "api/train");
 		var response = await Client.SendAsync(request);
 		if (response.StatusCode != System.Net.HttpStatusCode.OK)
@@ -51,5 +61,19 @@ public class LearningServiceClient : ILearningServiceClient
 		{
 			_logger.LogInformation("Model training triggered in learning service");
 		}
+	}
+
+	private async Task AssertAllPipelinesExecuted()
+	{
+		_logger.LogDebug("Asserting all pipelines executed (meaning datasets are ready)");
+		var pipelines = await _pipelinesDao.GetDtos();
+		var progress = 0;
+		foreach (var pipelineInfoDto in pipelines)
+		{
+			progress++;
+			await _pipelineExecutionService.ExecutePipelineSync(pipelineInfoDto.Id, skipIfExecuted: true);
+			_logger.LogDebug("Executed pipeline {Progress}/{Total}", progress, pipelines.Count);
+		}
+		_logger.LogInformation("All pipelines executed at least once");
 	}
 }
