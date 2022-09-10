0b8cbdfee38471a29d663d4ec47d6e1f4ff26ffe
Konstantin Strümpf <k.struempf@icloud.com>
2022-09-10 14:48:55 +0200

    fix: add missing migration files

---
+++ PipelineService/Migrations/20220910120038_StoreProcessingTime.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PipelineService.Migrations
+{
+    public partial class StoreProcessingTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "ProcessingDurationP",
+                table: "CandidateProcessingMetrics",
+                type: "double",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProcessingDurationP",
+                table: "CandidateProcessingMetrics");
+        }
+    }
+}
