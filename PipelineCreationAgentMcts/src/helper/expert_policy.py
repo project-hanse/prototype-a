55ed7d45908028ce5c73217416cbbeeca0256001
Konstantin Strümpf <k.struempf@icloud.com>
2024-01-04 16:23:59 +0100

    refactor: explicitly specify expert policy model for pipeline creation agents

--- PipelineCreationAgentMcts/src/helper/expert_policy.py
+++ PipelineCreationAgentMcts/src/helper/expert_policy.py
@@ -8,14 +8,15 @@ from src.config.config import get_api_secret, get_api_user, \
 from src.model.pipeline_state import PipelineBuildingState
 
 
-def model3_policy(state: PipelineBuildingState):
+def model_policy(state: PipelineBuildingState):
+	model_name = get_config('expert_policy_model_name')
 	while not state.is_terminal():
 		action = None
 		try:
 			if state.producing_operation is None or random.random() < get_config('expert_policy_probability'):
 				action = random.choice(state.get_possible_actions())
 			else:
-				suggested_op_identifiers = load_predicted_operation_identifier(state.producing_operation)
+				suggested_op_identifiers = load_predicted_operation_identifier(state.producing_operation, model_name)
 				for possible_action in state.get_possible_actions():
 					random.shuffle(suggested_op_identifiers)
 					for suggested_op_identifier in suggested_op_identifiers:
@@ -31,7 +32,7 @@ def model3_policy(state: PipelineBuildingState):
 	return state.get_reward()
 
 
-def load_predicted_operation_identifier(operation: dict) -> [str]:
+def load_predicted_operation_identifier(operation: dict, model_name: str) -> [str]:
 	payload = {
 		'feat_pred_count': 1,
 		'feat_pred_id': get_operation_identifier(operation)
@@ -39,7 +40,7 @@ def load_predicted_operation_identifier(operation: dict) -> [str]:
 	for i, input_datatype in zip(range(len(operation['outputTypes'])), operation['outputTypes']):
 		payload["input_%d_dataset_type" % i] = input_datatype
 	request = requests.post(
-		url=get_api_base_url_learning_service() + "/api/predict",
+		url=get_api_base_url_learning_service() + "/api/predict/%s" % model_name,
 		auth=(get_api_user(), get_api_secret()),
 		json=payload)
 	if request.status_code != 200:
